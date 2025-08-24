e9b309260f48b7f860a1b618d7742153086118bb
suomis7-netizen <suomis7@gmail.com>
2025-08-24 09:34:37 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true
-  },
-  // PWA configuration
-  async headers() {
-    return [
-      {
-        source: '/manifest.json',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/manifest+json',
-          },
-        ],
-      },
-    ];
-  },
+  }
 };
 
 module.exports = nextConfig;
